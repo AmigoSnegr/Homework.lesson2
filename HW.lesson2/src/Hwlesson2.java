d6507d8787e86b1481b52090834661d11bcf3363
AmigoSnegr <61880748+AmigoSnegr@users.noreply.github.com>
2020-04-13 13:58:10 +0200

    Add files via upload

---
+++ HW.lesson2/src/Hwlesson2.java
@@ -0,0 +1,16 @@
+public class Hwlesson2 {
+    public static void main(String[] args) {
+        /*int[] task1;
+        task1 = new int[2];
+        task1[0] = 1;
+        task1[1] = 0;*/
+
+        int[] task2;
+        task2 = new int[8];
+
+        for (i=0; i < 7; i++ || p=1; p<22; p + 3){
+        task2[i] = p;
+        System.out.println("task2[" + i + "] = " + task2[i]);
+        }
+    }
+}
